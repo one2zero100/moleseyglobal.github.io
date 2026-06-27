78ad146cc31be259086f2bca650c3d127baf5f3d
one2zero100 <one2zero100@gmail.com>
2026-06-27 12:57:11 +0100

    Delete assets/js/header-shrink.js

--- assets/js/header-shrink.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", () => {
-    const header = document.querySelector(".header");
-
-    if (window.scrollY > 40) {
-        header.classList.add("shrink");
-    } else {
-        header.classList.remove("shrink");
-    }
-});
