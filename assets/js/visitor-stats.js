26f108e23232149e4852895d46ebccd9f8ca6670
one2zero100 <one2zero100@gmail.com>
2026-07-08 19:41:10 +0100

    Remove Firebase config for SDK v7.20.0
    Removed Firebase configuration and initialization code for version 7.20.0.

--- assets/js/visitor-stats.js
+++ assets/js/visitor-stats.js
@@ -1,16 +1,3 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAQHDYIJDvN_s3TDzNI2nXuybV5WELvCDo",
-  authDomain: "molesey-global-counter.firebaseapp.com",
-  projectId: "molesey-global-counter",
-  storageBucket: "molesey-global-counter.firebasestorage.app",
-  messagingSenderId: "508365436436",
-  appId: "1:508365436436:web:f2e4f54dec2104d5445901",
-  measurementId: "G-2YNN2TDME7"
-};
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
 // ===============================
 // Firebase Modular SDK (v10+)
 // ===============================
